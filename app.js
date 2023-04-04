9ab388d9ea5ff44e8b827d9bcb252c06ace90a2e
21bowler <samwachira224@gmail.com>
2023-04-04 18:29:24 +0300

    first commit :zap:

---
+++ app.js
@@ -0,0 +1,16 @@
+const number = document.querySelector('.number');
+const dice = document.getElementById('dice');
+const adviceText = document.querySelector('.advice-text')
+
+
+
+// adding eventlistener to the button
+
+dice.addEventListener('click', showquote())
+
+
+// the callback function with its api
+
+function showquote(){
+    
+}
